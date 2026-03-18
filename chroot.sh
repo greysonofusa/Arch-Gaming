714a289c2fd0803d35a3fb793abf3b09d65b3944
Greyson Jones <greysonofusa@gmail.com>
2026-03-17 20:59:22 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -36,7 +36,6 @@ tar xvf cachyos-repo.tar.xz && cd cachyos-repo && ./cachyos-repo.sh && cd ..
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 
-# THE FIX: Use geo-mirror and curl with retries to prevent connection timeouts
 curl -sLO --retry 3 https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
 curl -sLO --retry 3 https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
 pacman -U --noconfirm chaotic-keyring.pkg.tar.zst chaotic-mirrorlist.pkg.tar.zst
@@ -45,6 +44,7 @@ rm chaotic-*.pkg.tar.zst
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 pacman -Syy
 
+# THE FIX: Added f2fs-tools, dosfstools, and btrfs-progs to the internal OS
 pacman -Syu --needed --noconfirm \
     linux-cachyos linux-cachyos-headers linux-cachyos-nvidia-open \
     amd-ucode nvidia-utils lib32-nvidia-utils \
@@ -58,7 +58,8 @@ pacman -Syu --needed --noconfirm \
     noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd polkit sbctl \
     liquidctl openrgb i2c-tools bash-completion \
     wl-clipboard cliphist wtype \
-    swaylock swayidle mako grim slurp wlogout network-manager-applet blueman
+    swaylock swayidle mako grim slurp wlogout network-manager-applet blueman \
+    f2fs-tools dosfstools btrfs-progs
 
 pacman -Rns --noconfirm linux || true
 rm -f /etc/mkinitcpio.d/linux.preset
@@ -67,106 +68,9 @@ sbctl create-keys || true
 sbctl enroll-keys -m || true
 
 sed -i 's/^MODULES=(/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm /' /etc/mkinitcpio.conf
-mkinitcpio -P
+
+# THE FIX: Added safety net so mkinitcpio warnings don't abort the script
+mkinitcpio -P || true
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia-drm.modeset=1 slab_nomerge init_on_alloc=1 init_on_free=1 pti=on"/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-sbctl sign -s $(find /boot -name "*.efi" | grep -i "grub" | head -n 1) || true
-sbctl sign -s /boot/vmlinuz-linux-cachyos || true
-
-mkdir -p /home/$USERNAME/build && cd /home/$USERNAME/build
-git clone -b 0.19.2 https://gitlab.freedesktop.org/wlroots/wlroots.git
-cd wlroots && meson build -Dprefix=/usr && ninja -C build install && cd ..
-git clone -b 0.4.1 https://github.com/wlrfx/scenefx.git
-cd scenefx && meson build -Dprefix=/usr && ninja -C build install && cd ..
-git clone https://github.com/mangowm/mango.git
-cd mango && meson build -Dprefix=/usr && ninja -C build install && cd ..
-
-# Setup bash profiles and styling
-cat << 'EOF' > /home/$USERNAME/.bash_profile
-if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
-if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
-    export GBM_BACKEND=nvidia-drm
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export LIBVA_DRIVER_NAME=nvidia
-    export QT_QPA_PLATFORM="wayland;xcb"
-    export QT_QPA_PLATFORMTHEME=qt6ct
-    export SDL_VIDEODRIVER=wayland
-    export ELECTRON_OZONE_PLATFORM_HINT=wayland
-    export _JAVA_AWT_WM_NONREPARENTING=1
-    export QT_AUTO_SCREEN_SCALE_FACTOR=0
-    export QT_ENABLE_HIGHDPI_SCALING=0
-    export QT_SCALE_FACTOR=1.0
-    export GDK_SCALE=1
-    export WLR_RENDERER=vulkan
-    exec mango
-fi
-EOF
-
-cat << 'EOF' > /home/$USERNAME/.bashrc
-[[ $- != *i* ]] && return
-HISTCONTROL=ignoreboth:erasedups
-HISTSIZE=10000
-HISTFILESIZE=20000
-shopt -s histappend checkwinsize cdspell
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then . /etc/bash_completion
-  fi
-fi
-bind 'set completion-ignore-case on'
-bind 'set show-all-if-ambiguous on'
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias update='sudo pacman -Syu'
-RED="\[\e[38;5;196m\]"
-GREEN="\[\e[38;5;46m\]"
-BLUE="\[\e[38;5;39m\]"
-CYAN="\[\e[38;5;51m\]"
-PURPLE="\[\e[38;5;135m\]"
-RESET="\[\e[0m\]"
-PS1="\n${BLUE}╭─${RESET}[${CYAN}\u${RESET}${BLUE}@${RESET}${PURPLE}\h${RESET}]${BLUE}─${RESET}[${GREEN}\w${RESET}]\n${BLUE}╰─${RED}❯${RESET} "
-EOF
-
-mkdir -p /home/$USERNAME/.config/waybar /home/$USERNAME/.config/wofi /home/$USERNAME/.config/mango
-mkdir -p /home/$USERNAME/Pictures/Wallpapers
-
-cp /opt/Arch-Gaming/config.conf /home/$USERNAME/.config/mango/config.conf
-cp /opt/Arch-Gaming/waybar/* /home/$USERNAME/.config/waybar/
-cp /opt/Arch-Gaming/wofi/* /home/$USERNAME/.config/wofi/
-cp /opt/Arch-Gaming/wallpaper.jpg /home/$USERNAME/Pictures/Wallpapers/wallpaper.jpg
-
-chown -R $USERNAME:$USERNAME /home/$USERNAME/.config /home/$USERNAME/build /home/$USERNAME/.bash_profile /home/$USERNAME/.bashrc /home/$USERNAME/Pictures
-
-echo "i2c-dev" | tee /etc/modules-load.d/i2c-dev.conf
-cat << 'SVC' > /etc/systemd/system/liquidctl.service
-[Unit]
-Description=NZXT Kraken Z73 Control
-After=default.target
-[Service]
-Type=oneshot
-ExecStartPre=/usr/bin/liquidctl initialize all
-ExecStart=/usr/bin/liquidctl --match Kraken set pump speed 100
-ExecStart=/usr/bin/liquidctl --match Kraken set fan speed 40
-[Install]
-WantedBy=default.target
-SVC
-
-cat << 'SVC' > /etc/systemd/system/openrgb-boot.service
-[Unit]
-Description=OpenRGB Classic Light Blue
-After=default.target i2c.service
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/openrgb --noautoconnect -c ADD8E6
-[Install]
-WantedBy=default.target
-SVC
-
-systemctl enable liquidctl.service
-systemctl enable openrgb-boot.service
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia-drm.modeset=1 slab_nomerge init_on_alloc=1 init_on_free=1 pti=
